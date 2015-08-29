87fa365b2c21ee9da9db813fc68ff5736286392c
lethe711 <dangki711@gmail.com>
2015-08-29 17:09:28 +1000

    ff

---
+++ tmp/2t0ims0051k4k.20himwbr85i8k.php
@@ -0,0 +1,10 @@
+    </div>
+    <?php if ($isMobile): ?>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
+        <?php else: ?><script src="http://code.jquery.com/jquery.js"></script>
+    <?php endif; ?>
+
+    <script src="../../ui/js/bootstrap.min.js"></script>
+
+    </body>
+</html>
